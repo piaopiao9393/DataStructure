15f3c4d7341415ef64bd8b161e0da4cd2740d25e
piaopiao9393 <zhoushengpu@126.com>
2018-12-05 23:13:41 +0800

    泛型数组array完成

--- src/array/Array.java
+++ src/array/Array.java
@@ -1,10 +1,13 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array<E> {
     private E[] data;
     //指向数组第一个没有元素的位置，相当于数组的末尾
     private int size;
-
+    //数组的容量
+    private int capacity;
     /*
     * 无参构造函数，capacity默认为10
     *
@@ -22,6 +25,7 @@ public class Array<E> {
     public Array(int capacity){
         data = (E[]) new Object[capacity];
         size = 0;
+        this.capacity = capacity;
     }
 
     /*
@@ -92,13 +96,66 @@ public class Array<E> {
         }
         data[index] = element;
     }
-    
+    /*
+    * 是否包含指定元素
+    *
+    * @param element 要查找的元素
+    * @return 是否包含的标志位
+    * */
+    public boolean contains(E element){
+        for(int i=0;i<size;i++){
+            if(data[i].equals(element)){
+                return true;
+            }
+        }
+        return false;
+    }
+    /*
+    * 查找指定元素
+    *
+    * @param element 要查找的元素
+    * @return 返回元素在data中的位置
+    * */
+    public int find(E element){
+        for(int i=0;i<size;i++){
+            if(data[i].equals(element)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    /*
+    * 删除index位置的元素，并返回删除的元素
+    *
+    * @param index 待删除的元素
+    * @return 删除元素的值
+    * */
+    public E remove(int index){
+        if(index<0 || index >= size){
+            throw new IllegalArgumentException("index的范围需要在[0,size)");
+        }
+        E ret = data[index];
+        for(int i=index+1;i<size;i++){
+            data[index] = data[index+1];
+        }
+        size--;
+        data[size-1] = null;
+        return null;
+    }
     private void resize(int newCapacity){
         E[] newData = (E[]) new Object[newCapacity];
         for(int i=0;i<size;i++){
             newData[i] = data[i];
         }
         data = newData;
+    }
+
+    @Override
+    public String toString(){
+        return "Array"+
+                "capacity="+ capacity+
+                ",size="+size+
+                ",data="+ Arrays.toString(data);
     }
 
 }
