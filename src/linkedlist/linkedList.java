b145d38d310d20fa316e72f5d8fcc33a945c8379
piaopiao9393 <zhoushengpu@126.com>
2018-12-07 00:08:42 +0800

    链表开个头

---
+++ src/linkedlist/linkedList.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+/*
+*  实现：1. 单链表反转
+*        2. 链表中环的检测
+*        3. 有序链表的合并
+*        4. 删除链表倒数第n个节点
+*        5. 求链表的中间节点
+* */
+public class linkedList {
+    public static class Node{
+        private int data;
+        private Node next;
+        
+    }
+}
